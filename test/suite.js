6b93c9bc665fe88dec8a7f16b8e547564850faf1
Bajix <Thomas@bajix.com>
2015-08-20 20:32:29 -0700

    Utilize mongoose-fixture-factory for stub models, fixtures

--- test/suite.js
+++ test/suite.js
@@ -1,7 +1,12 @@
 process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
 process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
+var fixtures = require('mongoose-fixture-factory/lib/fixtures'),
+  models = require('mongoose-fixture-factory/lib/models'),
+  Factory = require('mongoose-fixture-factory'),
+  chai = require('chai');
 
 global.assert = chai.assert;
 global.expect = chai.expect;
+
+Factory.register(models, fixtures);
