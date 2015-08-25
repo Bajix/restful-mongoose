2c3e5ba60675672c21b98ef785188d4a6f1acdbc
Bajix <Thomas@bajix.com>
2015-08-25 16:57:51 -0700

    Sanitize query select to maintain consistency

--- test/unit/plugins.js
+++ test/unit/plugins.js
@@ -14,8 +14,7 @@ Vottu();
 describe('Plugins', function() {
   before(Factory.fill);
   beforeEach(function() {
-    var vottu = new Vottu('User'),
-      app = express();
+    var app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
@@ -24,123 +23,160 @@ describe('Plugins', function() {
 
     app.use(validator());
 
-    vottu.use(function() {
-      this.pre('query', function() {
-        this.query.sort('createdAt');
-      });
-    });
-
-    app.get('/users.:format?', vottu.index().exec());
-    app.get('/users/:id.:format?', vottu.show().exec());
-
-    app.use(errorHandler);
-
     this.app = app;
 
     this.agent = request.agent(app);
   });
 
-  describe('Pagination', function() {
-    it('validates limit', function( done ) {
-      var uri = url.format({
-        pathname: '/users',
-        query: {
-          limit: 0
-        }
-      });
+  describe('Helpers', function() {
 
-      this.agent.get(uri)
-        .expect('Content-Type', /json/)
-        .expect(400)
-        .end(done);
-    });
+    describe('Select', function() {
+      it('sanitizes select', function( done ) {
+        var index = new Vottu('User').index(),
+          app = this.app;
 
-    it('validates skip', function( done ) {
-      var uri = url.format({
-        pathname: '/users',
-        query: {
-          skip: 0
-        }
-      });
-
-      this.agent.get(uri)
-        .expect('Content-Type', /json/)
-        .expect(400)
-        .end(done);
-    });
-
-    it('validates page', function( done ) {
-      var uri = url.format({
-        pathname: '/users',
-        query: {
-          p: 0
-        }
-      });
-
-      this.agent.get(uri)
-        .expect('Content-Type', /json/)
-        .expect(400)
-        .end(done);
-    });
-
-    it('paginates documents', function( done ) {
-      var users = fixtures.User.sort(function( a, b ) {
-        return Date.parse(a.createdAt) - Date.parse(b.createdAt);
-      });
-
-      var uri = url.format({
-        pathname: '/users',
-        query: {
-          limit: 1,
-          p: users.length
-        }
-      });
-
-      this.agent.get(uri)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function( err, res ){
-          if (err) {
-            return done(err);
-          }
-          assert.lengthOf(res.body, 1);
-          assert.propertyVal(res.body[0], '_id', users[users.length -1]._id);
-          done();
+        index.on('validate', function( req ) {
+          req.checkQuery('select.admin').equals(1);
         });
+
+        app.get('/users.:format?', index.exec());
+
+        app.use(errorHandler);
+
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            select: 'admin'
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(done);
+      });
     });
   });
 
-  describe('Select', function() {
-    it('selects fields', function( done ) {
-      var users = fixtures.User.sort(function( a, b ) {
-        return Date.parse(a.createdAt) - Date.parse(b.createdAt);
-      });
+  describe('Query Filters', function() {
+    beforeEach(function() {
+      var vottu = new Vottu('User'),
+        app = this.app;
 
-      var uri = url.format({
-        pathname: '/users',
-        query: {
-          select: '+createdAt -name'
-        }
-      });
-
-      this.agent.get(uri)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function( err, res ){
-          if (err) {
-            return done(err);
-          }
-
-          var users = res.body;
-
-          for (var i = 0; i < users.length; i++) {
-            var user = users[i];
-            assert.property(user, 'createdAt');
-            assert.notProperty(user, 'name');
-          }
-
-          done();
+      vottu.use(function() {
+        this.pre('query', function() {
+          this.query.sort('createdAt');
         });
       });
+
+      app.get('/users.:format?', vottu.index().exec());
+      app.get('/users/:id.:format?', vottu.show().exec());
+
+      app.use(errorHandler);
+    });
+
+    describe('Pagination', function() {
+      it('validates limit', function( done ) {
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            limit: 0
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .end(done);
+      });
+
+      it('validates skip', function( done ) {
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            skip: 0
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .end(done);
+      });
+
+      it('validates page', function( done ) {
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            p: 0
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .end(done);
+      });
+
+      it('paginates documents', function( done ) {
+        var users = fixtures.User.sort(function( a, b ) {
+          return Date.parse(a.createdAt) - Date.parse(b.createdAt);
+        });
+
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            limit: 1,
+            p: users.length
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function( err, res ){
+            if (err) {
+              return done(err);
+            }
+            assert.lengthOf(res.body, 1);
+            assert.propertyVal(res.body[0], '_id', users[users.length -1]._id);
+            done();
+          });
+      });
+    });
+
+    describe('Select', function() {
+      it('selects fields', function( done ) {
+        var users = fixtures.User.sort(function( a, b ) {
+          return Date.parse(a.createdAt) - Date.parse(b.createdAt);
+        });
+
+        var uri = url.format({
+          pathname: '/users',
+          query: {
+            select: '+createdAt -name'
+          }
+        });
+
+        this.agent.get(uri)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function( err, res ){
+            if (err) {
+              return done(err);
+            }
+
+            var users = res.body;
+
+            for (var i = 0; i < users.length; i++) {
+              var user = users[i];
+              assert.property(user, 'createdAt');
+              assert.notProperty(user, 'name');
+            }
+
+            done();
+          });
+        });
+    });
   });
 });
